c07c8a6b9f20a9f998ec81d1120268ff34339dec
PuppyPi <puppyofcodez@gmail.com>
2022-05-28 21:35:50 +0000

    Initial commit!  :D It's just a stub; we'll fill it out later XD'

---
+++ src/rebound/richdatashets/impls/helpers/gds/RichdatashetsGDSTranscoder.java
@@ -0,0 +1,18 @@
+package rebound.richdatashets.impls.helpers.gds;
+
+import rebound.exceptions.GenericDataStructuresFormatException;
+import rebound.richdatashets.api.model.RichdatashetsTable;
+
+public class RichdatashetsGDSTranscoder
+{
+	public static RichdatashetsTable decode(Object gds) throws GenericDataStructuresFormatException
+	{
+		
+	}
+	
+	
+	public static Object encode(RichdatashetsTable data)
+	{
+		
+	}
+}
